6dc0fce90e94baf38db71a2ad43ab49eaa60cfcd
shilonglong <7121867@qq.com>
2021-10-29 00:56:07 +0800

    config

--- client/config.py
+++ client/config.py
@@ -1,5 +1,14 @@
+import os
+
 version = "1.2"
 
-user_address = "0x88ef6c3de9060ab57af79dfda14128652e964fde"
-private_key = "32928f2d6fbd6c7496ee2467ac8287eb06952050e50a815d10fe392cad9176a0"
-access_token = "GHC9nQxmo1MyQIqk4woD9w=="
+env = os.environ
+
+
+def is_dev():
+    return env.get("ENV") == "dev"
+
+
+user_address = "0x88ef6c3de9060ab57af79dfda14128652e964fde" if is_dev() else ""
+private_key = "32928f2d6fbd6c7496ee2467ac8287eb06952050e50a815d10fe392cad9176a0" if is_dev() else ""
+access_token = "GHC9nQxmo1MyQIqk4woD9w==" if is_dev() else ""
